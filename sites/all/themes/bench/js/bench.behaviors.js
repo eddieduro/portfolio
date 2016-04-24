98d78831c69545340558bb2e82480742ccb77c81
eddie <e.jamesduro@gmail.com>
2016-04-24 16:43:20 -0700

    update hamburger menu color

--- sites/all/themes/bench/js/bench.behaviors.js
+++ sites/all/themes/bench/js/bench.behaviors.js
@@ -1,10 +1,11 @@
 (function ($) {   
   $(document).ready(function(){
-  var timeline     = new TimelineMax({delay:1.0}),
-      site_name    = $('.site-name'),
-      site_slogan  = $('.site-slogan');
-  $('.site-name').each(function (index) {
-    var characters = $.trim($(this).text());
+  let timeline     = new TimelineMax({delay:1.0}),
+      $site_name    = $('.site-name'),
+      $site_slogan  = $('.site-slogan');
+
+  $site_name.each(function (index) {
+    let characters = $.trim($(this).text());
     characters = characters.split("");
     $this = $(this);
     $this.empty();
@@ -14,15 +15,20 @@
     });
   });
 
+  let $hamburger = $(".hamburger");
+  $hamburger.on("click", function(e) {
+    $hamburger.toggleClass("is-active");
+    // Do something else, like open/close menu
+  });
   $(window).load(function(){
 
-  timeline.staggerFromTo($('.site-name').find("span"), 
+  timeline.staggerFromTo($site_name.find("span"), 
                         1, 
                         {autoAlpha: 0}, 
                         {autoAlpha: 1, ease: Back.easeOut}, 
                         0.1 );  
   timeline.add(
-    TweenMax.fromTo(site_slogan, 
+    TweenMax.fromTo($site_slogan, 
                     1, 
                     { y: -30, autoAlpha: 0, transformPerspective: 600, rotationX: -90 }, 
                     { y: 0, autoAlpha: 1, rotationX: 0, ease: Back.easeOut }, 0.1)
