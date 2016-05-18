ebf41720d8f3ed47d61f0f68a448a5cb10f39890
eddieduro <e.jamesduro@gmail.com>
2016-05-18 11:47:03 -0700

    change anchor links

--- sites/all/themes/bench/js/bench.js
+++ sites/all/themes/bench/js/bench.js
@@ -1,8 +1,28 @@
 (function($) {
   $(function() {
-  var $smoothScroll = $('.smoothScroll');
+  var $smoothScroll = $('.smoothScroll'),
+      $hamburger    = $(".hamburger"),
+      $menu         = $("#block-system-main-menu"),
+      timeline      = new TimelineMax({delay:1.0});
 
   $smoothScroll.click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000); // The number here represents the speed of the scroll in milliseconds
+        return false;
+      }
+    }
+    });
+  $('a').click(function(event) {
+    TweenMax.set($menu, {autoAlpha: 0});
+    $hamburger.toggleClass("is-active");
+    
+    event.preventDefault();
+
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
